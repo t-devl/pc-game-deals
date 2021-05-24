e2bc2ea6fe37885c0f64c16f9f503c97a20d0532
t-devl <79603858+t-devl@users.noreply.github.com>
2021-05-24 22:12:00 +0100

    Add form and display game search results

---
+++ src/components/Home.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+export default function Home() {
+  const [title, setTitle] = useState("");
+  const [games, setGames] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`https://www.cheapshark.com/api/1.0/games?title=${title}`)
+      .then((res) => res.json())
+      .then((data) => setGames(data));
+  };
+
+  return (
+    <div className="home" onSubmit={handleSubmit}>
+      <form className="home__form form">
+        <div className="form__title">
+          <label className="form__label">Title</label>
+          <input
+            className="form__input"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          ></input>
+        </div>
+        <button className="form__button">Search Games</button>
+      </form>
+      <div className="home__games-container">
+        {games.map((game) => (
+          <div className="home__game game" key={game.gameID}>
+            <div className="game__cover">
+              <img className="game__image" src={game.thumb} alt=""></img>
+            </div>
+            <div className="game__details">
+              <h2 className="game__title">{game.external}</h2>
+              <a className="game__view-deals" href="#">
+                See Deals
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
